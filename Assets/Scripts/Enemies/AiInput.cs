6627d7bd8b91906253e222fece4dd9755e78745c
Ruslan Kibets <ruslan.kibets@gmail.com>
2022-04-02 14:59:22 -0700

    Moving Enemies

--- Assets/Scripts/Enemies/AiInput.cs
+++ Assets/Scripts/Enemies/AiInput.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class AiInput : MonoBehaviour
 {
+    [SerializeField] private float holdPositionTime;
+    [SerializeField] private float holdPositionTimeSpread;
+    [SerializeField] private float newPositionRadius;
+
+    private const int NewPositionAttempts = 5;
+    private const float PositionReachRadius = 1.5f;
+    
     private PanHero _character;
 
     private PanHero _target;
+
+    private float holdPositionTimer;
+
+    private Vector3 _targetPos;
     
     private void Awake()
     {
+        _targetPos = transform.position;
         _character = GetComponent<PanHero>();
     }
 
@@ -28,6 +40,44 @@ public class AiInput : MonoBehaviour
             // TODO raycast visibility, bursts, etc
             if(Random.value > 0.7f) _character.HoldTrigger();
         }
-  
+
+        holdPositionTimer -= Time.deltaTime;
+
+        if (holdPositionTimer <= 0)
+        {
+            holdPositionTimer = holdPositionTime + Random.Range(-holdPositionTimeSpread, holdPositionTimeSpread);
+
+            FindNewPosition();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position, _targetPos) > PositionReachRadius)
+        {
+            _character.FixedMove((_targetPos - transform.position).normalized);
+        }
+    }
+
+    private void FindNewPosition()
+    {
+        var mask = LayerMask.GetMask("Ground");
+
+        for (int i = 0; i < NewPositionAttempts; i++)
+        {
+            var angle = Random.value * Mathf.PI * 2f;
+            var x = Mathf.Cos(angle) * newPositionRadius;
+            var y = Mathf.Sin(angle) * newPositionRadius;
+
+            var candidate = transform.position + new Vector3(x, y + 5f, 0); // +5 so always above the pan
+            
+            var raycast = Physics.Raycast(candidate, -Vector3.up, out var hit, 10f, mask);
+
+            if (raycast)
+            {
+                _targetPos = hit.point;
+                break;
+            }
+        }
     }
 }
