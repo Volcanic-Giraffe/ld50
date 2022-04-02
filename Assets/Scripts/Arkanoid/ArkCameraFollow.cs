794e4a26caab428815c4dd2d3c291c08480d04f9
Ruslan Kibets <ruslan.kibets@gmail.com>
2022-04-01 22:00:34 -0700

    Reversenoid prototype (2)

---
+++ Assets/Scripts/Arkanoid/ArkCameraFollow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArkCameraFollow : MonoBehaviour
+{
+    public Transform Target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target != null)
+        {
+            var pos = transform.position;
+
+            transform.position = new Vector3(pos.x, Target.transform.position.y, pos.z);
+        }
+    }
+}
