5cc68ba12abf633c81a4e1156dc93d639661f34b
Ruslan Kibets <ruslan.kibets@gmail.com>
2022-04-02 14:15:23 -0700

    Sliding bullet

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float Altitude;
+    
     public BulletConfig Config { get; private set; }
 
     private Rigidbody _rig;
 
     private float _speed;
+
+    private int _floorMask;
     
     private void Awake()
     {
+        _floorMask = LayerMask.GetMask("Ground");
+
         _rig = GetComponent<Rigidbody>();
     }
 
@@ -23,7 +29,20 @@ public class Bullet : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rig.velocity = transform.forward * (_speed * Time.fixedDeltaTime);
+        var newPos = transform.position + (transform.forward * (Time.fixedDeltaTime * _speed));
+
+        var raycast = Physics.Raycast(newPos, -Vector3.up, out var hit, 10f, _floorMask);
+
+        if (raycast)
+        {
+            _rig.MovePosition(hit.point + Vector3.up * Altitude);
+            
+            // Debug.DrawLine (newPos, hit.point, Color.red);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
