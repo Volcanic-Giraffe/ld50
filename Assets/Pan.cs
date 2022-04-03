f5463eef2c3e92589787e4c90e20b84e8aaa7781
Ruslan Kibets <ruslan.kibets@gmail.com>
2022-04-03 16:16:20 -0700

    Pan intensity transition fix

--- Assets/Pan.cs
+++ Assets/Pan.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -5,44 +6,60 @@ using Random = UnityEngine.Random;
 public class Pan : MonoBehaviour
 {
     [SerializeField] private PanHeat heat;
-    private float _originY;
-
+    [SerializeField] private float heatIncrementY;
+    [SerializeField] private float heatMaxLocalY;
+    
     public PanHeat PanHeat => heat;
-    private float intensity = 0.5f;
+    private float intensity = 1.0f;
     
     private Rigidbody _rig;
 
-
+    private float _originPanY;
+    private float _originHeatLocalY;
+    private float _targetHeatLocalY;
+    
     private void Awake()
     {
         _rig = GetComponent<Rigidbody>();
-        _originY = transform.position.y;
+        _originPanY = transform.position.y;
+        _originHeatLocalY = heat.transform.localPosition.y;
+
+        _targetHeatLocalY = _originHeatLocalY;
     }
-    
-    public void IncreaseHeat()
-    {
-        PanHeat.SetRadius(PanHeat.Radius + 2);
-        PanHeat.SetGlow(intensity);
-        intensity += 0.5f;
-        PanHeat.transform.DOMoveY(PanHeat.transform.position.y + 1, 2f);
-    }
-    
+
     private void Update()
     {
         if (Input.GetButtonDown("Jump"))
         {
             PanFlip();
         }
+        if (Input.GetKeyDown("t"))
+        {
+            IncreaseHeat();
+        }
+
+    }
+    
+    public void IncreaseHeat()
+    {
+        if (_targetHeatLocalY >= heatMaxLocalY) return;
+
+        _targetHeatLocalY += heatIncrementY;
+        
+        PanHeat.SetRadius(PanHeat.Radius + 2);
+        intensity += 0.2f;
+        PanHeat.SetGlow(intensity);
+        PanHeat.transform.DOLocalMoveY(_targetHeatLocalY, 2f);
     }
 
     public void PanFlip()
     {
-        _rig.DOMoveY(_originY+ 3f, 1.4f).SetEase(Ease.InBack).OnComplete(
+        _rig.DOMoveY(_originPanY+ 3f, 1.4f).SetEase(Ease.InBack).OnComplete(
             () =>
             {
                 FlipProps();
                 
-                _rig.DOMoveY(_originY, 0.7f).SetEase(Ease.OutBack);
+                _rig.DOMoveY(_originPanY, 0.7f).SetEase(Ease.OutBack);
             });
     }
 
